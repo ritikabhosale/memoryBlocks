cd51351be215795d1c2f9eb9127a26d8b1405b78
Ritika Bhosale <54773240+ritikabhosale@users.noreply.github.com>
2022-05-14 13:37:59 +0530

    Created the base structure of the game

---
+++ play.sh
@@ -0,0 +1,13 @@
+read -p "Enter players name: " playerName
+
+node ./resources/setUp.js $playerName
+
+gameStatus=$( grep "isGameOver" ./resources/data.json | grep "false" )
+
+while [[ $gameStatus ]]
+do
+    read -p "Enter your pick: " pickOfPlayer
+    node memoryBlocks.js $pickOfPlayer
+    
+    gameStatus=$( cat resources/data.json | grep "isGameOver" | grep "false" )
+done
---
+++ resources/setUp.js
@@ -0,0 +1,43 @@
+const fs = require('fs');
+const stringify = JSON.stringify;
+
+const duplicate = arr => arr.concat(arr);
+
+const writeToFile = (file, content) => fs.writeFileSync(file, content, 'utf8');
+
+const shuffle = function (arr) {
+  return arr;
+};
+
+const symbolObject = function (symbol, index) {
+  return {
+    position: index + 1,
+    symbol: symbol,
+    reveal: false
+  };
+};
+
+const setBlocks = function (symbols, numOfBlocks) {
+  const uniqSymbols = symbols.slice(0, numOfBlocks / 2);
+  const shuffledSymbols = shuffle(duplicate(uniqSymbols));
+  return shuffledSymbols.map(symbolObject);
+};
+
+const gameSetUp = function (playerName, numOfBlocks, symbols) {
+  const data = {
+    board: setBlocks(symbols, numOfBlocks),
+    player: { name: playerName, score: 0 },
+    lastMove: null,
+    isGameOver: false
+  };
+  writeToFile('./resources/data.json', stringify(data))
+};
+
+const symbols = [
+  '&#9214', '&#9835',
+  '&#9851', '&#9733',
+  '&#9730', '&#9787',
+  '&#9775', '&#9752'
+];
+
+gameSetUp(process.argv[2], 16, symbols);
